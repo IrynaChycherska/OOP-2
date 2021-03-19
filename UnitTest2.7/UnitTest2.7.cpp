269ce7eb3cfe691622d668217adf66e372db28cc
unknown <iryna.chycherska.itis.2020@lpnu.ua>
2021-03-19 20:59:12 +0200

    2.7

---
+++ UnitTest2.7/UnitTest2.7.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../2.7/Point.h"
+#include"../2.7/Point.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest27
+{
+	TEST_CLASS(UnitTest27)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Point c;
+			Assert::IsTrue(c.Vidstan() == sqrt(c.GetX() * c.GetX() + c.GetY() * c.GetY()));
+		}
+	};
+}
